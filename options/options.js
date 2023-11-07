efb110730c8c63674221189c714e6f1b62bf3ca8
Crowdbotics <team@crowdbotics.com>
2023-11-07 16:48:39 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "prd-demo-healthcare-44391",
+  url: "https://prd-demo-healthcare-44391.botics.co",
+  api: "https://prd-demo-healthcare-44391.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
